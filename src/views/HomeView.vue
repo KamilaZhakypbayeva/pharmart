3a0fe50bcd301e374e1b990ba19b94b7939d51ff
Kamila <zhakypbaeva18@gmail.com>
2022-07-15 12:52:47 +0600

    HomeView.vue, popularCategories, Bestsellers

---
+++ src/views/HomeView.vue
@@ -0,0 +1,490 @@
+<template>
+    <img class="location" src="./../assets/location.png"/>
+    <h1 class="Алматы_Казахстан">Алматы, Казахстан</h1>
+    <h1 class="Стать_партнером">Стать партнером</h1>
+    <h1 class="Аптеки">Аптеки</h1>
+    <h1 class="Рус">Рус</h1>
+    <img class="Russia" src="./../assets/Russia.png"/>
+    <h2 class="Pharmart">Pharmart</h2>
+    <img class="logo_pharm1" src="./../assets/logo_pharm1.png"/>
+    <button class="katalog"><img src="./../assets/Component.png" alt="" style="vertical-align:middle"> Каталог
+    </button>
+    <form>
+    <input type="text" placeholder="Поиск" class="poisk">
+    <input type="button" >
+    <img class="search_img" src="./../assets/search.png"/>
+    </form>
+    <h1 class="корзина">Корзина</h1>
+    <img class="корзина_img" src="./../assets/корзина.png"/>
+    <img class="user" src="./../assets/login.png"/>
+    <img class="heart" src="./../assets/heart.png"/>
+    <h2 class="Избранные ">Избранные</h2>
+    <h2 class="Бексултан ">Бексултан</h2>
+    <img class="slider1" src="./../assets/back1.png"/>
+    <h1 class="slider1_h1">Что такое Lorem Ipsum?</h1>
+    <p class="slider1_p">Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой"</p>
+    <img class="slider1_img" src="./../assets/Аптечка2.png"/>
+    <input name="button_buy" type="button" value="Перейти">
+    <div class="med_teh">
+    <h1>Мед.техника</h1></div>
+    <img class="med_teh_img" src="./../assets/med_teh.png"/>
+
+</template>
+<script>
+</script>
+<style scoped>
+
+@font-face {
+    font-family: 'Inter'; /* Гарнитура шрифта */
+    src: url(./../fonts/Inter/Inter-VariableFont_slnt\,wght.ttf); /* Путь к файлу со шрифтом */
+   }
+.location {
+position: absolute;
+height: 16.094757080078125px;
+width: 13.333330154418945px;
+left: 67.33px;
+top: 17.5px;
+border-radius: [object Object]px;
+}
+.Алматы_Казахстан {
+position: absolute;
+width: 132px;
+height: 17px;
+left: 89px;
+top: 14px;
+border-radius: nullpx;
+
+/* Text/Regular */
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 11px;
+/* identical to box height */
+
+
+/* Text/Grey 400 */
+
+color: #626262;
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;
+}
+.Стать_партнером {
+position: absolute;
+width: 121px;
+height: 17px;
+left: 272px;
+top: 11.5px;
+/* Tertiary button/Medium */
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 500;
+font-size: 14px;
+line-height: 17px;
+/* identical to box height */
+
+
+/* Text/Grey 400 */
+
+color: #626262;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+.Аптеки{
+position: absolute;
+width: 50px;
+height: 17px;
+left: 425px;
+top: 11.5px;
+/* Tertiary button/Medium */
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 500;
+font-size: 14px;
+line-height: 17px;
+/* identical to box height */
+
+
+/* Text/Grey 400 */
+
+color: #626262;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;
+}
+.Рус {
+    position: absolute;
+width: 25px;
+height: 17px;
+left: 1330px;
+top: 13px;
+
+/* Tertiary button/Medium */
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 500;
+font-size: 14px;
+line-height: 17px;
+/* identical to box height */
+
+
+/* Text/Grey 400 */
+
+color: #626262;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+.Russia{
+position: absolute;
+left: 1360px;
+top: 23.8px;
+width: 16px;
+height: 12px;
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+}
+.Pharmart {
+position: absolute;
+width: 173px;
+height: 42px;
+left: 116px;
+top: 69px;
+font-family: 'Inter';
+font-style: normal;
+font-weight: 700;
+font-size: 35px;
+line-height: 42px;
+letter-spacing: 0.05em;
+color: #2AC1CE;
+}
+.logo_pharm1 {
+position: absolute;
+height: 42.000003814697266px;
+width: 42.006935119628906px;
+left: 64px;
+top: 95px;
+border-radius: 0px;
+
+}
+.katalog {
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding-left: 45px;
+gap: 10px;
+text-align: right;
+background-color: #FE8326;
+border-color: #FE8326;
+border-radius: 8px;
+position: absolute;
+width: 125px;
+height: 46px;
+margin-left: 384px;
+top: 95.5px;
+bottom: 0%;
+font-family: 'Inter';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+color: #FFFFFF;
+
+/* Inside auto layout */
+}
+
+
+.katalog img {
+position: absolute;
+width: 20px;
+height: 20px;
+/* Inside auto layout */
+left:17.67px;
+flex: none;
+order: 0;
+flex-grow: 0;
+ }
+
+input[type="text"] {
+box-sizing: border-box;
+position: absolute;
+width: 510px;
+height: 46px;
+margin-left: 533px;
+top: 95.4px;
+justify-content: center;
+background: #FFFFFF;
+border: 1px solid #04B2B0;
+text-align: left;
+border-radius: 10px;
+ }
+input[type="button"] {
+position: absolute;
+text-align: left;
+left: 983px;
+top: 95px;
+color: #A9A9A9;
+width: 84px;
+height:46px;
+background: #AFE7E6;
+border: 10px red;
+border-radius: 10px;
+}
+.poisk{
+position: absolute;
+width: 49px;
+height: 19px;
+left: 24px;
+top: 13px;
+text-align: left;
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+
+/* Text/Grey 300 */
+
+color: #A9A9A9;
+}
+.search_img {
+position: absolute;
+height: 18.66666603088379px;
+width: 18.66666603088379px;
+position: absolute;
+left: 1013.67px;
+top: 107px;
+bottom: 3.33%;
+
+}
+.корзина{
+position: absolute;
+left: 1096px;
+top: 115px;
+
+width: 57px;
+height: 17px;
+
+/* Text/Regular */
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 17px;
+/* identical to box height */
+color: #2C2C2C;
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+}
+.корзина_img {
+position: absolute;
+height: 22.399999618530273px;
+width: 22.399999618530273px;
+border-radius: [object Object]px;
+left: 1111.3px;
+top: 95px;
+}
+.heart {
+position: absolute;
+left: 1217.8px;
+top: 99.4px;
+right: 70px;
+height: 19.19999885559082px;
+width: 22.399999618530273px;
+}
+.Избранные {
+position: absolute;
+width: 75px;
+height: 19px;
+left: 1190px;
+top: 112.3px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 19px;
+
+color: #000000;
+}
+.Бексултан {
+position: absolute;
+width: 75px;
+height: 19px;
+left: 1305px;
+top: 112.3px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 19px;
+
+color: #000000;
+}
+.user {
+position: absolute;
+height: 22.38541030883789px;
+width: 16px;
+left: 1332.5px;
+top: 97.8px;
+}
+.slider1{
+position: absolute;
+width: 1312.3px;
+height: 460px;
+left: 64px;
+top: 162px;
+}
+.slider1_h1 {
+position: absolute;
+width: 632px;
+height: 58px;
+left: 118px;
+top: 274px;
+
+/* Header/H1 */
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 600;
+font-size: 48px;
+line-height: 58px;
+/* identical to box height */
+
+
+/* Brand/Blue 700 */
+
+color: #157594;
+}
+.slider1_p {
+position: absolute;
+width: 577px;
+height: 48px;
+left: 151px;
+top: 346px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 24px;
+/* or 133% */
+/* Brand/Blue 700 */
+
+color: #157594;
+}
+.slider1_img {
+position: absolute;
+width: 482px;
+height: 279px;
+left: 804px;
+top: 227px;
+}
+
+input[name="button_buy"]{
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 16px 32px;
+gap: 10px;
+
+position: absolute;
+width: 133px;
+height: 51px;
+left: 150px;
+top: 426px;
+
+/* Brand/Orange 500 */
+background: rgba(254, 131, 38, 1);
+border-radius: 8px;
+font-family: 'Inter';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 19px;
+
+color: #FFFFFF;
+}
+.med_teh{
+position: absolute;
+width: 270px;
+height: 269px;
+left: 64px;
+top: 868px;
+
+/* Text/ White */
+
+background: #FFFFFF;
+border-radius: 20px;
+box-shadow: 0px 1px 16px 0px rgba(0, 0, 0, 0.1);
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+.med_teh:hover {
+background: linear-gradient(147.28deg, #5AE1E1 -35.63%, #D4FCF2 151.87%);
+}
+.med_teh h1 {
+position: absolute;
+width: 228px;
+height: 60px;
+left: 16px;
+top: 175px;
+
+/* Header/H3 */
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 600;
+font-size: 24px;
+line-height: 24px;
+/* or 100% */
+
+text-align: center;
+
+/* Brand/Blue 700 */
+
+color: #157594;
+}
+
+.med_teh_img {
+position: absolute;
+height: 119.2784423828125px;
+width: 184.0317840576172px;
+left:145px;
+top: 932px;
+border-radius: 0px;
+
+
+}
+</style>
